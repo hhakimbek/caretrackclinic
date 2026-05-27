e7ae290bf218f9db506bc826fc19027c714ab14a
hakimbek <hakimbekmrx@gmail.com>
2026-05-27 22:26:33 +0500

    mongo db deleted

--- backend/src/data/db.js
+++ backend/src/data/db.js
@@ -4,25 +4,25 @@ export const users = [];
 export const refreshTokens = [];
 
 export const patients = [
-  {
+  /*{
     id: uuidv4(),
     fullName: "Ali Valiyev",
     phone: "+998901112233",
     dateOfBirth: "1995-04-12",
     gender: "male",
     createdAt: new Date().toISOString(),
-  },
+  },*/
 ];
 
 export const doctors = [
-  {
+  /*{
     id: uuidv4(),
     fullName: "Dr. Dilnoza Karimova",
     specialty: "Cardiologist",
     phone: "+998935554433",
     status: "Active",
     createdAt: new Date().toISOString(),
-  },
+  },*/
 ];
 
 export const appointments = [];
